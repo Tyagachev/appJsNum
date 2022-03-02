9a906055e65db7d4d4e852daa70398387c769440
Tyagachev Anton <570uz@mail.ru>
2022-03-02 12:25:51 +0300

    +

--- burger.js
+++ burger.js
@@ -5,7 +5,7 @@ let divBurg = document.querySelector('.burgermenu');
 let clickZhmyak = document.querySelector('.zhmyak');
 clickZhmyak.addEventListener('click',function(){
         for(let i = 0; i <=100; i++){
-            divBurg.style.height = i + 'px';
+        divBurg.style.height = i + 'px';
         divBurg.classList.toggle('burburger__menu-off');
         clickZhmyak.style.display = 'none';
         clickZhmyakAlt.style.display = 'block';
