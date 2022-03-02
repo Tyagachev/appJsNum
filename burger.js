eaa47d402a9e085203edf13c938bf8519fc40b11
Tyagachev Anton <570uz@mail.ru>
2022-03-02 14:05:19 +0300

    Добавил плавности открытия

--- burger.js
+++ burger.js
@@ -5,12 +5,18 @@ let divBurg = document.querySelector('.burgermenu');
 let clickZhmyak = document.querySelector('.zhmyak');
 clickZhmyak.addEventListener('click',function(){
         for(let i = 0; i <= 100; i++){
+                setTimeout(function () {
         divBurg.style.height = i + 'px';
+        }, i * 3,5);
+                setTimeout(function (){
         divBurg.classList.toggle('burburger__menu-off');
+        },20);
+}
         clickZhmyak.style.display = 'none';
         clickZhmyakAlt.style.display = 'block';
-        }
+        
 });
+
 
 // Иконка крестика
 let clickZhmyakAlt = document.querySelector('.zhmyak__alt');
